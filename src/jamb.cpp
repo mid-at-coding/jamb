4c204598f2943934f16a670fcf461973489d7d5a
emelia <emaleksan@gmail.com>
2026-04-08 09:13:24 -0400

    C rewrite in progress

--- src/jamb.cpp
+++
@@ -1,9 +0,0 @@
-#include "audioplayable.hpp"
-#include "library.hpp"
-
-int main()
-{
-	Library<AudioPlayable, AudioMetaData, AudioPlayable::acceptable> mylib("/home/emelia/Music/Human People - Butterflies Drink Turtle Tears/");
-	printf("Press Enter to quit...");
-	getchar();
-}
